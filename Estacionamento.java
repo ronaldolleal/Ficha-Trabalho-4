867623cdb21908d0f032fb52b18219f6828084f5
JorgeFreitas7 <133856325+JorgeFreitas7@users.noreply.github.com>
2024-10-01 14:20:21 +0100

    Novos metedos

--- Estacionamento.java
+++ Estacionamento.java
@@ -1,4 +1,8 @@
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Estacionamento {
 
     public static void main(String[] args) {
@@ -66,6 +70,82 @@ public class Estacionamento {
             }
 
         }
+
+    }
+
+    private void entrarEstacionamento(Scanner scanner) {
+
+        if (lugaresOcupados < lotacao_maxima) {
+
+            System.out.print("Digite o seu nome: ");
+
+            String nome = scanner.nextLine();
+
+            System.out.print("Digite a matrícula do carro: ");
+
+            String matricula = scanner.nextLine();
+
+            System.out.print("Digite o tempo de estacionamento em minutos: ");
+
+            int tempo = scanner.nextInt();
+
+            scanner.nextLine(); // Limpa o buffer 
+
+            double valorAPagar = calcularValor(tempo);
+
+            System.out.println("Valor a pagar: €" + valorAPagar);
+
+            alunos.add(new Aluno(nome, matricula, tempo, valorAPagar));
+
+            lugaresOcupados++;
+
+            salvarEstacionamento(nome, matricula, tempo, valorAPagar);
+
+        } else {
+
+            System.out.println("O parque está cheio. Não é possível entrar.");
+
+        }
+
+    }
+
+    private void sairEstacionamento(Scanner scanner) {
+
+        System.out.print("Digite a matrícula do carro: ");
+
+        String matricula = scanner.nextLine();
+
+        boolean encontrado = false;
+
+        for (Aluno aluno : alunos) {
+
+            if (aluno.getMatricula().equals(matricula)) {
+
+                System.out.println("Você saiu do estacionamento.");
+
+                alunos.remove(aluno);
+
+                lugaresOcupados--;
+
+                encontrado = true;
+
+                break;
+
+            }
+
+        }
+
+        if (!encontrado) {
+
+            System.out.println("Matrícula não encontrada.");
+
+        }
+
+    }
+
+    private double calcularValor(int minutos) {
+
+        return Math.ceil(minutos / 15.0) * tarifa;
 
     }
 }
